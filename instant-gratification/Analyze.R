bee4127e05defcd2989f02a439e3a1d53e04136a
Gaborotta <10.daaaan.24@gmail.com>
2019-06-20 11:38:35 +0900

    L1正則化を導入

--- instant-gratification/Analyze.R
+++ instant-gratification/Analyze.R
@@ -2,9 +2,14 @@ library(data.table)
 library(dplyr)
 library(ROCR)
 library(parallel)
-library(tidyr)
+library(glmnet)
 
-setwd("C:/Users/10daa/Documents/workspase/kaggle/instant-gratification")
+#データ読み込み
+print(paste(Sys.time(),"Load Data",sep = " : "))
+train<-fread("./input/train.csv")
+test<-fread("./input/test.csv")
+submission<-fread("./input/sample_submission.csv")
+train<-select(train,-id)
 
 #ロジスティック回帰
 #データの5割を学習データとして、25回繰り返し計算し、予測値の平均値を求める。
@@ -16,12 +21,22 @@ LR<-function(i){
   result_data0<-test1%>%mutate(fit=0)
   for(j in 1:count){
     #学習
-    train0<-train1%>%sample_frac(size = 0.5)
-    result = glm(target ~., data=train0, family=binomial(link="logit"))
+    #glmnetでL1正則化
+    train0<-train1%>%sample_frac(size = 1-1/count)
+    train_x<-as.matrix(train0%>%select(-target))
+    train_y<-as.matrix(train0%>%select(target))
+    result = glmnet(train_x,train_y,family="binomial",alpha=1,lambda = 0.05)
+    #result = glmnet(target ~., data=train0, family=binomial(link="logit"))
     
     #予測
-    sample_data0<-sample_data0%>%mutate(fit=fit+predict(result,train1,type="response")/count)
-    result_data0<-result_data0%>%mutate(fit=fit+predict(result,test1,type="response")/count)
+    new_train_x<-as.matrix(train1%>%select(-target))
+    train_predict<-predict(result,new_train_x,s=result$lambda,type='response')
+    sample_data0<-sample_data0%>%mutate(fit=fit+train_predict[,1]/count)
+    #sample_data0<-sample_data0%>%mutate(fit=fit+predict(result,train1,type="response")/count)
+    new_test_x<-as.matrix(test1%>%select(-id))
+    test_predict<-predict(result,new_test_x,s=result$lambda,type='response')
+    result_data0<-result_data0%>%mutate(fit=fit+test_predict[,1]/count)
+    #result_data0<-result_data0%>%mutate(fit=fit+predict(result,test1,type="response")/count)
   }
   sample_data0<-sample_data0%>%select(target,fit)
   result_data0<-result_data0%>%select(id,fit)
@@ -29,30 +44,27 @@ LR<-function(i){
 }
 
 #並列処理準備
-cores = detectCores(logical=FALSE)
+print(paste(Sys.time(),"Set Parallel",sep = " : "))
+cores = detectCores(logical=TRUE)
 cluster = makeCluster(cores, "PSOCK")
 clusterEvalQ(cluster,{
   library(data.table)
   library(dplyr)
   library(ROCR)
-  library(foreach)
-  library(doParallel)
+  library(glmnet)
 })
 clusterSetRNGStream(cluster,129)
-
-#データ読み込み
-train<-fread("train.csv")
-test<-fread("test.csv")
-train<-select(train,-id)
 clusterExport(cluster,"train")
 clusterExport(cluster,"test")
 
 #並列処理実行
 num<-0:511
+print(paste(Sys.time(),"Run Parallel",sep = " : "))
 system.time(par_data <- parLapply(cluster,num,LR))
 stopCluster(cluster)
 
 #出力データ整理
+print(paste(Sys.time(),"Shape Data",sep = " : "))
 train_data<-data.table()
 test_data<-data.table()
 for (i in 1:512) {
@@ -61,12 +73,15 @@ for (i in 1:512) {
 }
 
 #trainのAUC計算
+print(paste(Sys.time(),"Calc Train AUC",sep = " : "))
 pred <- prediction(train_data$fit, train_data$target)
 auc.tmp <- performance(pred,"auc")
 auc <- as.numeric(auc.tmp@y.values)
 print(auc)
 
 #結果ファイル出力
-submission<-fread("sample_submission.csv")
+print(paste(Sys.time(),"Output File",sep = " : "))
 submission<-submission%>%left_join(test_data,by="id")%>%select(id,fit)%>%rename(target=fit)
-write.csv(submission,"submission.csv",row.names = F)
+write.csv(submission,"submission_005.csv",row.names = F)
+
+print(paste(Sys.time(),"Finish",sep = " : "))
